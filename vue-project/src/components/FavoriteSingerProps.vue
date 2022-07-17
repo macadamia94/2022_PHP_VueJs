4a3e0c05649590f1b1099c80c49e87149734bef3
macadamia94 <thswlgus15@hanmail.net>
2022-07-17 19:03:08 +0900

    add vue-project folder

---
+++ vue-project/src/components/FavoriteSingerProps.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="imgsize" v-bind:src="imgSrc">
+</template>
+
+<script>
+export default {
+  props: {
+    imgSrc: String,
+  }
+}
+</script>
+
+<style scoped>
+.imgsize {
+  width: 200px;
+}
+</style>
---
+++ vue-project/src/dao/TodoDao.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export default {
+  getList: function (fn) {
+    axios.get("/todo/index").then((res) => {
+      if (res.status === 200) {
+        console.log(res.data);
+        fn(res.data);
+      }
+    });
+  },
+};
