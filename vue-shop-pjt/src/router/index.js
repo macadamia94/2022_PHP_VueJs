0b6412b9b39a735fb9d60149d30c8885f17ece59
macadamia94 <thswlgus15@hanmail.net>
2022-07-19 13:17:43 +0900

    로그인 경고창 (requireAuth)

--- vue-shop-pjt/src/router/index.js
+++ vue-shop-pjt/src/router/index.js
@@ -5,6 +5,20 @@ import SalesList from '../views/SalesList';
 import ProductCreate from '../views/ProductCreate';
 import ProductUpdate from '../views/ProductUpdate';
 import ImageInsert from '../views/ImageInsert';
+import store from '@/store';
+import swal from 'sweetalert2';
+
+//네비게이션 가드
+const requireAuth = () => (to, from, next) => {
+  // (가고자하는 위치, 현재 위치, next)
+  // 로그인이 필요한 위치를 체크해서 쓸 것이기 때문에 to, from은 사용하지 않음
+  if (store.state.user.iuser === undefined) {
+    // undefined인 경우 → 로그인을 안 한 경우
+    swal.fire('로그인을 하세요.', '', 'warning');
+    return;
+  }
+  return next(); // next가 호출이 되어야 사용가능
+};
 
 const routes = [
   {
@@ -21,6 +35,9 @@ const routes = [
     path: '/create',
     name: 'ProductCreate',
     component: ProductCreate,
+    beforeEnter: requireAuth(),
+    // 로그인이 필요한 위치 체크
+    // beforeEnter : 일자로 흐르는 물길을 우회해서 흐를 수 있게 해줌
   },
   {
     path: '/update',
